98c32f8f86530742ee2a20248f4f28d367b6f220
Daniel Hollick <dan.hollick@gmail.com>
2018-10-08 12:27:21 +0200

    filter out frames

--- server.js
+++ server.js
@@ -16,8 +16,39 @@ async function figmaFileFetch(projectId){
 
     let figmaFileStruct = await result.json()
 
+    let figmaFrames = figmaFileStruct.document.children
+        .filter(child => child.type === 'CANVAS')[0].children
+        .filter(child => child.type === 'FRAME')
+    // console.log(JSON.stringify(figmaFrames))
+        .map(frame => {
+            return {
+                name: frame.name,
+                id: frame.id
+            }
+        })
+
+    // console.log(JSON.stringify(figmaFrames))
+
+    // let ids = figmaFrames.map(comp => comp.id).join(',')
+    // console.log(JSON.stringify(ids))
+
+    // let imageResult = await fetch('https://api.figma.com/v1/images/' + figmaId + '?scale=3&ids=' + ids, {
+    //     method: 'GET',
+    //     headers: {
+    //         'X-Figma-Token': FigmaAPIKey
+    //     }
+    // }).catch(error => console.log(error))
+
+    // let figmaImages = await imageResult.json().catch(error => console.log(error))
+    // console.log(JSON.stringify(figmaImages))
+    // figmaImages = figmaImages.images
+    // console.log(JSON.stringify(figmaImages))
+
+
     return figmaFileStruct
 }
+
+
 
 app.use('/', async function (req, res, next) {
     let result = await figmaFileFetch(FigmaFileID).catch(error => console.log(error))
