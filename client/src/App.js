1f4db3a74992196b5972714c62b0681af1454efa
Daniel Hollick <dan.hollick@gmail.com>
2018-10-09 14:41:15 +0200

    display in react

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import './App.css'
+
+var containerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: 24,
+  height: '100vh',
+}
+
+var divStyle = {
+  margin: 24,
+}
+
+var imgStyle = {
+  maxHeight: 200,
+}
+
+class App extends Component {
+  state = { images: [
+    { name: '', url:''  }
+  ]
+}
+
+ componentDidMount() {
+    fetch('/test')
+      .then(res => res.json())
+      .then(data => this.setState({ images: data }))
+      .then(console.log(this.state))
+      .catch(error => console.log(error));
+  }
+
+
+  render() {
+    return (
+      <div className="App" style={containerStyle}>
+        {this.state.images.map(
+          (frame,i) =>
+          <div key={i} style={divStyle}>
+            <img src={frame.url} style={imgStyle} alt={"figma component"}/>
+            <p>{frame.name}</p>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const FigmaAPIKey = process.env.API_KEY
 const FigmaFileID = 'VAyAHaZn1tHmjOFK79pbnMTj'
 
-async function figmaFileFetch(projectId){
-    let result = await fetch('https://api.figma.com/v1/files/' + projectId , {
+async function figmaFileFetch(fileId){
+    let result = await fetch('https://api.figma.com/v1/files/' + fileId , {
         method: 'GET',
         headers: {
             'X-Figma-Token': FigmaAPIKey
@@ -27,32 +27,33 @@ async function figmaFileFetch(projectId){
             }
         })
 
-
-
     let ids = figmaFrames.map(comp => comp.id).join(',')
 
-
-    let imageResult = await fetch('https://api.figma.com/v1/images/' + FigmaFileID + '?scale=3&ids=' + ids, {
+    let imageResult = await fetch('https://api.figma.com/v1/images/' + fileId + '?scale=2&ids=' + ids, {
         method: 'GET',
         headers: {
             'X-Figma-Token': FigmaAPIKey
         }
     }).catch(error => console.log(error))
 
-    let figmaImages = await imageResult.json().catch(error => console.log(error))
+    let figmaImages = await imageResult.json()
 
     figmaImages = figmaImages.images
-    console.log(JSON.stringify(figmaImages))
 
 
-    return figmaFileStruct
+    return figmaFrames.map(frame => {
+        return {
+            name: frame.name,
+            url: figmaImages[frame.id]
+        }
+    })
 }
 
 
-
-app.use('/', async function (req, res, next) {
+app.use('/test', async function (req, res, next) {
     let result = await figmaFileFetch(FigmaFileID).catch(error => console.log(error))
-    res.send(JSON.stringify(result))
+    console.log(result)
+    res.send(result)
 })
 
 app.listen(3001, console.log("Holy shit, I'm a server and I am listening on port 3001"))
