ea3de998e07fc925f1d3537ab8d8a439e34a1b3d
Daniel Hollick <dan.hollick@gmail.com>
2018-10-12 09:31:02 +0200

    remove console logs

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
     fetch('/frames')
       .then(res => res.json())
       .then(data => this.setState({ images: data }))
-      .then(console.log(this.state))
       .catch(error => console.log(error));
   }
 
